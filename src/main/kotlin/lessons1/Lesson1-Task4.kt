cafeb96ceb4cd03d00b236c22d8167c286768449
AndrewQJLIN <nut01@yandex.ru>
2023-03-25 17:26:47 +0400

    KS_1_4 типа данных в космосе

---
+++ src/main/kotlin/lessons1/Lesson1-Task4.kt
@@ -0,0 +1,18 @@
+package lessons1
+
+fun main() {
+    val lenght: Long = 40_868_600_000L
+    val age: UShort = 27u
+    val partOfDay: Float = 0.075f
+    val second: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(second)
+    println(partOfYear)
+    println(apogee)
+
+}
