88489ac6dde56293a69cf994b94aef875a430929
AndrewQJLIN <nut01@yandex.ru>
2023-03-25 17:11:51 +0400

    KS_1_2 первый заход

---
+++ src/main/kotlin/lessons1/Lesson1-Task2.kt
@@ -0,0 +1,13 @@
+package lessons1
+
+fun main() {
+    val countOfOrders: Int = 75
+    val goodBayText: String = "Благодарим за покупку"
+
+    println(countOfOrders)
+    println(goodBayText)
+
+    var countOfStaff: Int = 2000
+    println(countOfStaff)
+
+}
