0784cfd1f550cfdbdbdbf14e9cee1e4614fd7af9
AndrewQJLIN <nut01@yandex.ru>
2023-03-25 19:06:25 +0400

    KS_2_1

---
+++ src/main/kotlin/lessons2/Lesson2_Task1.kt
@@ -0,0 +1,13 @@
+package lessons2
+
+fun main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val countOfStudents: Float = 4f
+
+    println("Средний балл:")
+    println((student1 + student2 + student3 + student4) / countOfStudents)
+}
