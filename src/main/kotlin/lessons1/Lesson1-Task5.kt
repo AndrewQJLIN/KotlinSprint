bab81ddc78481ee9e550124ca592a2d0512a4af6
AndrewQJLIN <nut01@yandex.ru>
2023-03-28 21:36:21 +0400

    KS_1_5 - использовал format и поставил ведущий 0 впереди секунд, если их меньше 10

--- src/main/kotlin/lessons1/Lesson1-Task5.kt
+++ src/main/kotlin/lessons1/Lesson1-Task5.kt
@@ -5,14 +5,16 @@ fun main() {
     val minutes = second / 60
     val secOfEnd = second % 60
 
-    if (secOfEnd == 0) {
-        println("$minutes:00")
-        return
-    }
+//    if (secOfEnd == 0) {
+//        println("$minutes:00")
+//        return
+//    }
+//
+//    if (secOfEnd < 10) {
+//        println("$minutes:0$secOfEnd")
+//    } else {
+//        println("$minutes:$secOfEnd")
+//    }
 
-    if (secOfEnd < 10) {
-        println("$minutes:0$secOfEnd")
-    } else {
-        println("$minutes:$secOfEnd")
-    }
+    println(String.format("%d:%02d",minutes,secOfEnd))
 }
