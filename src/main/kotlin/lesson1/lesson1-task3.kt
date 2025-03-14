990eacada8dfcf2495532e649276705a42a3e348
Андрей Кужилин <nut01@yandex.ru>
2025-03-14 12:35:40 +0400

    KS-1-3 gagarin

---
+++ src/main/kotlin/lesson1/lesson1-task3.kt
@@ -0,0 +1,26 @@
+package lesson1
+
+fun main() {
+    val yearOfStart = 1961
+    var hour = "09"
+    var minute = "07"
+
+    print("Год полета - ")
+    println(yearOfStart)
+
+    print("Взлет - ")
+    print(hour)
+    print(":")
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print("Посадка - ")
+    print(hour)
+    print(":")
+    println(minute)
+
+
+
+}
