e1b41aa66f0b796cc08bd31cc5ee2d705a7c3418
Андрей Кужилин <nut01@yandex.ru>
2025-03-14 15:35:55 +0400

    KS-1-5 cosmos v.3

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -1,14 +1,14 @@
 package lesson1
 
 const val SECOND_IN_MINUTES = 60
-const val MINUTES_IN_HOUR = 3600
+const val SECOND_IN_HOUR = SECOND_IN_MINUTES* SECOND_IN_MINUTES
 fun main() {
 
 
     val secondOfTotal = 6480
-    val hour = secondOfTotal / (MINUTES_IN_HOUR)
-    val minutes = (secondOfTotal - hour * MINUTES_IN_HOUR) / SECOND_IN_MINUTES
-    val second = secondOfTotal - (hour * MINUTES_IN_HOUR + minutes * SECOND_IN_MINUTES)
+    val hour = secondOfTotal / (SECOND_IN_HOUR)
+    val minutes = (secondOfTotal - hour * SECOND_IN_HOUR) / SECOND_IN_MINUTES
+    val second = secondOfTotal - (hour * SECOND_IN_HOUR + minutes * SECOND_IN_MINUTES)
 
     println(String.format("%02d:%02d:%02d", hour, minutes, second))
 }
