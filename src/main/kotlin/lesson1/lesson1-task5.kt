66266e3a8f2fc5b069169aa9d59e5e08b114f2cb
Андрей Кужилин <nut01@yandex.ru>
2025-03-14 14:19:28 +0400

    KS-2-5 deposit v.2

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -1,10 +1,14 @@
 package lesson1
 
+const val SECOND_IN_MINUTES = 60
+const val MINUTES_IN_HOUR = 3600
 fun main() {
-    val secondOfTotal = 6480
-    val hour = secondOfTotal/3600
-    val minutes = (secondOfTotal - hour*3600)/60
-    val second = secondOfTotal - (hour*3600 + minutes*60)
 
-    println(String.format("%02d:%02d:%02d",hour,minutes,second))
+
+    val secondOfTotal = 6480
+    val hour = secondOfTotal / (MINUTES_IN_HOUR)
+    val minutes = (secondOfTotal - hour * MINUTES_IN_HOUR) / SECOND_IN_MINUTES
+    val second = secondOfTotal - (hour * MINUTES_IN_HOUR + minutes * SECOND_IN_MINUTES)
+
+    println(String.format("%02d:%02d:%02d", hour, minutes, second))
 }
