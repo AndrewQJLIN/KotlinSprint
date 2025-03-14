6ab93c747bf0e367da122bb4b23017c66a337627
Андрей Кужилин <nut01@yandex.ru>
2025-03-14 12:34:50 +0400

    KS-1-2 staff

---
+++ src/main/kotlin/lesson1/lesson1-task2.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main() {
+    val numberOfOrders = 75
+    val thanksForPurchase = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thanksForPurchase)
+
+    var countOfStaff = 2000
+    // println(countOfStaff)
+    countOfStaff--
+    println(countOfStaff)
+
+}
