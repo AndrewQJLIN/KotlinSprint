1067138a1c425ef2b3a06ddcff8420204921d2c2
Андрей Кужилин <nut01@yandex.ru>
2025-03-14 15:24:40 +0400

    KS-3-1 greeting

---
+++ src/main/kotlin/lesson3/lesson3-task1.kt
@@ -0,0 +1,11 @@
+package lesson3
+
+fun main() {
+    val userName = "Andrew"
+    var greetingText = "Good day"
+
+    println("$greetingText!, $userName")
+
+    greetingText = "Good evening"
+    println("$greetingText!, $userName")
+}
