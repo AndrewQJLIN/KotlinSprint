3c16bc9ac339d0e1d7d71cc7508b0d0e9cf71986
Андрей Кужилин <nut01@yandex.ru>
2025-03-15 15:17:37 +0400

    KS-2-3 train v.2

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -1,15 +1,16 @@
 package lesson2
 
+const val MINUTES_IN_HOUR = 60
 fun main() {
     val startHour = 9
     val startMinute = 39
     val travelTime: Int = 457
 
-    val startTimeInMinutes = startHour * 60 + startMinute
+    val startTimeInMinutes = startHour * MINUTES_IN_HOUR + startMinute
     val endTimeInMinutes = startTimeInMinutes + travelTime
 
-    val endHour = endTimeInMinutes / 60
-    val endMinute = endTimeInMinutes % 60
+    val endHour = endTimeInMinutes / MINUTES_IN_HOUR
+    val endMinute = endTimeInMinutes % MINUTES_IN_HOUR
 
     println("Отправление: $startHour:$startMinute")
     println("Прибытие   : $endHour:$endMinute")
