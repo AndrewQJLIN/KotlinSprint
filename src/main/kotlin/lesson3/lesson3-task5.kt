0b7c3ecfbe88066d81ddc3e867727ca6a7615b1c
Андрей Кужилин <nut01@yandex.ru>
2025-03-15 21:57:47 +0400

    KS-3-5 use split fun

--- src/main/kotlin/lesson3/lesson3-task5.kt
+++ src/main/kotlin/lesson3/lesson3-task5.kt
@@ -3,9 +3,13 @@ package lesson3
 fun main() {
     val playerMove = "D2-D4;0"
 
-    val startMove = playerMove.substringBefore('-')
-    val endMove = playerMove.substringAfter('-').substringBefore(';')
-    val countMove = playerMove.substringAfter(';')
+    val arrayData = playerMove.split("-",";")
+
+    val startMove = arrayData[0]
+    val endMove = arrayData[1]
+    val countMove = arrayData[2]
+
+
 
     println("Ход игрока номер: $countMove")
     println("Фигура перемещается с клетки: $startMove")
