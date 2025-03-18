c8c9f8ec5187d97c1634861dc3a67420678c9efd
Андрей Кужилин <nut01@yandex.ru>
2025-03-18 22:38:00 +0400

    KS-4-5 ship v.2

--- src/main/kotlin/lesson4/lesson4-Task5.kt
+++ src/main/kotlin/lesson4/lesson4-Task5.kt
@@ -1,21 +1,21 @@
 fun main() {
 
     println("Есть ли провреждения корпуса (напишите)?\nfalse - если НЕТ повреждений\ntrue - если ДА, ЕСТЬ повреждения")
-    val conditionStatus = readln().toBoolean()
+    val isDamageShip = readln().toBoolean()
     println("Введите численность экипажа (число):")
     val crewSize = readln().toInt()
     println("Введите число ящиков провизии (число):")
     val provisionBoxes = readln().toInt()
     println("Благоприятна ли погода (напишите)?\nfalse - если НЕТ, погода НЕ благоприятная\ntrue - если ДА, погода благоприятная")
-    val weatherStatusIsGood = readln().toBoolean()
+    val isWeatherIsGood = readln().toBoolean()
 
-    val readyToStart =
-        ((!conditionStatus && (crewSize >= MINIMUM_CREW_SIZE && crewSize < MAXIMUM_CREW_SIZE)) ||
-                (crewSize == MAXIMUM_CREW_SIZE && weatherStatusIsGood))
+    val isReadyToStart =
+        ((!isDamageShip && (crewSize >= MINIMUM_CREW_SIZE && crewSize < MAXIMUM_CREW_SIZE)) ||
+                (crewSize == MAXIMUM_CREW_SIZE && isWeatherIsGood))
                 &&
                 provisionBoxes > MINIMUM_PROVISION_BOXES
 
-    println("При введенных данных отправка корабля: $readyToStart")
+    println("При введенных данных отправка корабля: $isReadyToStart")
 }
 
 const val MINIMUM_CREW_SIZE = 55
