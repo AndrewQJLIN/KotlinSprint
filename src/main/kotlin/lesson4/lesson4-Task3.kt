d8a449d1fa030d4b8385c612ec2fcf1147cc038f
Андрей Кужилин <nut01@yandex.ru>
2025-03-18 22:33:17 +0400

    KS-4-3 agro v.2

--- src/main/kotlin/lesson4/lesson4-Task3.kt
+++ src/main/kotlin/lesson4/lesson4-Task3.kt
@@ -1,18 +1,21 @@
 fun main() {
 
     val isSunny = true
-    val tentOpen = true
+    val isAwningOpen = true
     val humidityValue = 20
     val seasonCurrent = "ЗИМА"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunny==NEED_SUNNY)
-            && (tentOpen==TENT_OPEN) 
-            && (humidityValue == HUMIDITY_IN_PERCENT) 
-            && seasonCurrent != INCORRECT_SEASON}")
-
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? ${
+            (isSunny == IS_SUNNY)
+                    && (isAwningOpen == IS_AWNING_OPEN)
+                    && (humidityValue == HUMIDITY_IN_PERCENT)
+                    && seasonCurrent != INCORRECT_SEASON
+        }"
+    )
 }
 
-const val NEED_SUNNY = true
-const val TENT_OPEN = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val HUMIDITY_IN_PERCENT = 20
 const val INCORRECT_SEASON = "ЗИМА"
