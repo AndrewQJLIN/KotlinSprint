941b933df6de4e6c72dcf9dbe233e8c9cbebd273
Андрей Кужилин <nut01@yandex.ru>
2025-03-20 21:43:40 +0400

    KS-5-6 imt v.2

--- src/main/kotlin/lesson5/lesson5-Task6.kt
+++ src/main/kotlin/lesson5/lesson5-Task6.kt
@@ -7,20 +7,17 @@ fun main() {
     println("Введите массу тела в килограммах: ")
     val weight = readln().toDouble()
     println("Введите рост в сантиметрах: ")
-    val length = readln().toDouble() /100
+    val length = readln().toDouble() / 100
 
     val imt = weight / length.pow(2)
 
-    val result: String = if (imt < 18.5) {
-        "Недостаточная масса тела"
-    } else
-        if (imt < 25) {
-            "Нормальная масса тела"
-        } else
-            if (imt < 30) {
-                "Избуточная масса тела"
-            } else {
-                "Ожирение"
-            }
-    println(String.format("Ваш ИМТ = %.2f $result",imt))
+    val result: String = when (imt) {
+        in 0.0..<18.5 -> "Недостаточная масса тела"
+        in 18.5..<25.0 -> "Нормальная масса тела"
+        in 25.0..<30.0 -> "Избыточная масса тела"
+        else -> {
+            "Ожирение"
+        }
+    }
+    println(String.format("Ваш ИМТ = %.2f $result", imt))
 }
